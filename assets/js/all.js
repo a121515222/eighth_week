bcf8c2b2da39b0c73370ec7c561cbcc7819db5cc
a121515222 <85506228+a121515222@users.noreply.github.com>
2021-08-25 02:49:20 +0800

    Update 2021-08-24T18:49:06.079Z

---
+++ assets/js/all.js
@@ -0,0 +1,17 @@
+"use strict";
+
+$(function () {
+  console.log('Hello Bootstrap5');
+}); //swiper
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  breakpoints: {
+    992: {
+      slidesPerView: 1,
+      spaceBetween: 30
+    }
+  }
+});
+//# sourceMappingURL=all.js.map
