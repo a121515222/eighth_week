06dc13aaee604be4ac4ea687b05df43ec99d54a2
a121515222 <85506228+a121515222@users.noreply.github.com>
2021-09-01 17:19:59 +0800

    Update 2021-09-01T09:19:57.346Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -166,7 +166,25 @@ $(function () {
       }
     });
   }
-}); //first-experience
+}); //loader-when finshed load remove the icon
+
+$(window).on("load", function () {
+  $(".loader").remove();
+}); //vanillajs-datepicker
+
+var elem = document.querySelector('input[name="datepicker"]');
+
+if (elem) {
+  var datepicker = new Datepicker(elem, {
+    nextArrow: '>',
+    prevArrow: '<',
+    buttonClass: 'btn datepicker-primary',
+    autohide: true,
+    clearBtn: true // format: 'mm/dd/yyyy',
+
+  });
+} //first-experience
+
 
 $('.first-experience').click(function (e) {
   $('.card-first-experience').toggleClass('active');
@@ -183,38 +201,36 @@ var advanced = document.querySelector('.chooseCourse-advanced');
 var courseLevel = document.querySelector('.courseLevel'); //getAttribute
 //remove the hide class
 
-function present(e) {
-  e.preventDefault();
-  show.classList.remove('hide');
-  show.classList.add('display');
+if (show) {
+  var present = function present(e) {
+    e.preventDefault();
+    show.classList.remove('hide');
+    show.classList.add('display');
+  };
+
+  var wbasic = function wbasic() {
+    courseLevel.innerHTML = "基礎";
+  };
+
+  var wintermediate = function wintermediate() {
+    courseLevel.innerHTML = "中階";
+  };
+
+  var wadvanced = function wadvanced() {
+    courseLevel.innerHTML = "進階";
+  };
+
+  if (basic) {
+    var _wbasic = function _wbasic() {
+      courseLevel.innerHTML = "基礎";
+    };
+  }
+
+  basic.addEventListener('click', present, false);
+  basic.addEventListener('click', wbasic, false);
+  intermediate.addEventListener('click', present, false);
+  intermediate.addEventListener('click', wintermediate, false);
+  advanced.addEventListener('click', present, false);
+  advanced.addEventListener('click', wadvanced, false);
 }
-
-function wbasic() {
-  courseLevel.innerHTML = "基礎";
-}
-
-function wintermediate() {
-  courseLevel.innerHTML = "中階";
-}
-
-function wadvanced() {
-  courseLevel.innerHTML = "進階";
-}
-
-basic.addEventListener('click', present, false);
-basic.addEventListener('click', wbasic, false);
-intermediate.addEventListener('click', present, false);
-intermediate.addEventListener('click', wintermediate, false);
-advanced.addEventListener('click', present, false);
-advanced.addEventListener('click', wadvanced, false); //vanillajs-datepicker
-
-var elem = document.querySelector('input[name="datepicker"]');
-var datepicker = new Datepicker(elem, {
-  nextArrow: '>',
-  prevArrow: '<',
-  buttonClass: 'btn datepicker-primary',
-  autohide: true,
-  clearBtn: true // format: 'mm/dd/yyyy',
-
-});
 //# sourceMappingURL=all.js.map
